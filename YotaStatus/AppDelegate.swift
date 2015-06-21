a0b7eec883fe7d2bed96a752eb57bca3bfb4f056
Roman <zummo3@gmail.com>
2015-06-21 10:47:18 +0300

    Initial Commit

---
+++ YotaStatus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YotaStatus
+//
+//  Created by Roman Faizullin on 21.06.15.
+//  Copyright (c) 2015 Roman Faizullin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
